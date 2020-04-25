45ffc2c0c10afe2c641fc06951382c375e534e08
pouletmortuaire <joedev010@gmail.com>
2020-04-25 15:30:33 +0700

    added rows and calls to results table

--- static/myscript.js
+++ static/myscript.js
@@ -35,11 +35,8 @@ function change2(x1) {
     while (tbody1.hasChildNodes()) {
         tbody1.removeChild(tbody1.childNodes[0])  
         }
-
-        
     console.log("out while");
 
-    //add the new <td>
     
     for(i = 1; i <= y; i++){ console.log("in for"); 
         let table1 = document.getElementById('tbody');
@@ -57,36 +54,112 @@ function change2(x1) {
         let table5 = document.createElement('input');  //create a <input> node
         table5.type = "number";
         table5.id = "pic" + i;
-        let id5 = "pic" + i;
+        //let id5 = "pic" + i;
         //table5.addEventListener("input", TRr(id5))
         table4.appendChild(table5);
+
+        //add the Aplha row
+        let alpha1 = document.createElement("th");
+        alpha1.scope = "row";
+        table2.appendChild(alpha1);
+        let alpha2 = document.createElement('td');
+        alpha2.id = "alpha1" + i; 
+        alpha2.placeholder = "alpha " + i;
+        alpha2.classname = "form-control";
+        alpha1.appendChild(alpha2);
+        let alpha3 = document.createElement('input');  //create a <input> node
+        alpha3.type = "number";
+        alpha3.id = "Alpha" + i;
+        alpha2.appendChild(alpha3);
 
         //add the TRr row
         let trrow = document.createElement("th");
         trrow.scope = "row";
-        trrow.innerHTML = i;
         table2.appendChild(trrow);
         let trrow2 = document.createElement('td');
         trrow2.id = "TRr" + i; 
         trrow2.placeholder = "TRr" + i;
         trrow2.classname = "form-control";
-        table2.appendChild(trrow2);
+        trrow2.innerHTML = "XXXX";
+        trrow.appendChild(trrow2);
+
+        //add the K' row
+        let Krow1 = document.createElement("th");
+        Krow1.scope = "row";
+        table2.appendChild(Krow1);
+        let Krow2 = document.createElement('td');
+        Krow2.id = "Kret" + i; 
+        Krow2.classname = "form-control";
+        Krow2.innerHTML = "Kret" + i;
+        Krow1.appendChild(Krow2);
+
+        //add the N row
+        let N1 = document.createElement("th");
+        N1.scope = "row";
+        table2.appendChild(N1);
+        let N2 = document.createElement('td');
+        N2.id = "N" + i; 
+        N2.classname = "form-control";
+        N2.innerHTML = "N" + i;
+        N1.appendChild(N2);
+
+        //add the s row
+        let S1 = document.createElement("th");
+        S1.scope = "row";
+        table2.appendChild(S1);
+        let S2 = document.createElement('td');
+        S2.id = "S" + i; 
+        S2.classname = "form-control";
+        S2.innerHTML = "S" + i;
+        S1.appendChild(S2);
+
+        //add the HEPT row
+        let HEPT1 = document.createElement("th");
+        HEPT1.scope = "row";
+        table2.appendChild(HEPT1);
+        let HEPT2 = document.createElement('td');
+        HEPT2.id = "HEPT" + i; 
+        HEPT2.classname = "form-control";
+        HEPT2.innerHTML = "HEPT" + i;
+        HEPT1.appendChild(HEPT2);
         }
+
         
-    for(let k = 1; k <= y; k++){ 
-        console.log("in for2 n°" + k);
-        $("input").change(
-            function () {
-                let picpic 
+    
+    for (let k = 1; k <= y; k++) {
+        $('#TRr'+k).ready(function () {
+            console.log("change from TR" + k);
+            $("#pic" + k).change(function () {
+                let picpic;
                 picpic = 'pic' + k;
                 let paramTRr;
                 paramTRr = document.getElementById(picpic);
-                $("#table5.id").change(TRr(paramTRr)); console.log("in JQuery1" + k); 
-                }
-            )
+                $("#pic".id).change(TRr(paramTRr, k),Kret(picpic, k), N(k));
+                })
+
+        //call N() when N change
+        $('#N'+k).ready(function () {
+            $("#Alpha" + k).change(function () {
+                $("#N"+ k).change(N(k), HEPT(k));
+                console.log("Alpha"+k +"changed!")
+                });    
+            })
+        
+        $('#S'+k).ready(function () {
+            $('#pic'+ k).change(function () {
+                $("#S"+ k).change(S(k));
+                console.log("Alpha"+k +"changed!")
+                });    
+            })
+        $('#N'+k).ready(function () {
+            $('#Lg').change(function () {
+                HEPT(k);
+                console.log("HEPT called from Lg change");
+                });    
+            })
+            })
         }
-   
-    }        
+    }
 function event1(y){ console.log(y)
     for(i = 1; i <= y; i++){
         console.log("in for n°2 "+i+" times, go for pic" + i);
@@ -122,35 +195,34 @@ function repeat(x){
     return text2, console.log("out of repeat2!")
     }
 
-function TRr(x) {
-    console.log("function TRr received:" + x);
-        if (x) {
-            let Tm = document.getElementById("TM").value;
-            console.log("Tm:" + Tm);
-            let TRrx = x.value - Tm
-            console.log("TRrx:" + TRrx);
-            document.getElementById("TRr" + x).innerHTML = TRrx
-            return console.log("TRrx is" + TRrx)
-            }
-        else return console.log("Can't get element by id with:" + x);;
-    }
+function TRr(x, y) {
+    if (x) {
+        let Tm = document.getElementById("TM").value;
+        console.log("Tm:" + Tm+" y:"+y);
+        let TRrx = x.value - Tm
+        document.getElementById("TRr" + y).innerHTML = TRrx;
+        return console.log("TRrx is" + TRrx)
+        }
+    else return console.log("Can't get element by id with:" + x);;
+}
 
-function Kret(TR) { console.log("in Kret")
+function Kret(TR, k) { console.log("in Kret")
     TR1 = document.getElementById(TR).value;
     Tm = document.getElementById("TM").value;
-    console.log("Tm: " + Tm +"TR1: "+TR1);
-    Kret1 = (TR1 - Tm)/Tm
-    document.getElementById("Kret"+TR).innerHTML = parseInt(Kret1)
-    return console.log("Kret is"+Kret1)
+    console.log("Tm: " + Tm +"TR1: " + TR1);
+    Kret01 = (TR1 - Tm)/Tm;
+    document.getElementById("Kret"+k).innerHTML = Kret01;
+    return console.log("Kret is" + Kret01);
     }
 
-function N(pic) { console.log("in N()")
-    Alpha = document.getElementById("alpha"+pic).value
+function N(pic) { 
+    console.log("in N()");
+    Alpha = document.getElementById("Alpha"+pic).value;
     TR1 = document.getElementById("pic"+pic).value;
     console.log("Tm: " + Tm +"TR1: "+TR1);
-    N1 = (5.54 *((TR1 / Alpha)*(TR1 / Alpha)))
-    document.getElementById("N"+pic).innerHTML = N1.toFixed(2)
-    return console.log("N1 is"+N1)
+    N1 = (5.54 *((TR1 / Alpha)*(TR1 / Alpha)));
+    document.getElementById("N"+pic).innerHTML = N1.toFixed(2);
+    return console.log("N1 is "+N1)
     }
 
 function S(pic) { console.log("in S()")
@@ -168,15 +240,18 @@ function S(pic) { console.log("in S()")
         }
     }
 
-function HEPT(idN,idL) { console.log("in S()")
-    N1 = document.getElementById(idN).value;
-    N1 = parseInt(N1)
-    console.log("N1: " + N1)
-    L1 = document.getElementById(idL).textContent;
-    L1 = parseInt(N2)
-    console.log("N1: " + N1 +"L1: "+L1);
-    H = L1 / N1
-    console.log("Hept"+idN)
-    document.getElementById("Hept"+idN).innerHTML = H.toFixed(2)
-    return console.log("H: "+H.toFixed(2))
+function HEPT(k) { console.log("in HEPT()")
+    let H1;
+    H1 = document.getElementById("Lg").value;
+    console.log("H1 is "+H1)
+    H1 = parseInt(H1)
+    console.log("H1 is "+H1)
+    let H2;
+    H2 = document.getElementById("N"+k).textContent;
+    H2 = parseFloat(H2)
+    console.log("H2 is "+H2)
+    H = H1 / H2
+    console.log("Hept = " + H +"from k="+k)
+    document.getElementById("HEPT"+k).innerHTML = H.toFixed(2)
+    return console.log("return HEPT function")
     }
